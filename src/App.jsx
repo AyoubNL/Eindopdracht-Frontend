12f5c0f625bc1c2c36a040986db5fea27a4d9313
AyoubNL <gmilio@live.nl>
2024-07-11 16:13:12 +0200

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import './styles/global.css'
-
-
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
+import Signin from "./pages/signin/Signin.jsx";
+import Signup from "./pages/signup/Signup.jsx";
+import Fleet from "./pages/fleet/Fleet.jsx";
+import Notfound from "./pages/notfound/Notfound.jsx";
 
 function App() {
 
     return (
         <>
-
-<p>{import.meta.env.VITE_API_KEY}</p>
-
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/signup' element={<Signup/>}/>
+                <Route path='/signin' element={<Signin/>}/>
+                <Route path='/fleet' element={<Fleet/>}/>
+                <Route path="*" element={<Notfound/>}/>
+            </Routes>
         </>
     )
 }
