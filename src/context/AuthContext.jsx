32d9bcdcb95a7ba025dc707e15baf1b047f6c4aa
AyoubNL <gmilio@live.nl>
2024-07-17 01:48:18 +0200

    Contextprovider

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,30 @@
+import {createContext, useState} from "react";
+
+
+export const AuthContext = createContext({});
+
+function AuthContextProvider({children}) {
+    const [isAuth, toggleIsAuth] = useState({
+        isAuth: false,
+        user: {
+            username: '',
+            email: '',
+            password: '',
+        },
+        status: 'pending'
+    });
+
+
+
+
+
+    return (
+        <AuthContext.Provider value={{isAuth, toggleIsAuth}}>
+
+            {children}
+
+        </AuthContext.Provider>
+    );
+}
+
+export default AuthContextProvider;
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -2,10 +2,46 @@ import './Signup.css'
 import user_icon from "../../assets/icons/person.png";
 import email_icon from "../../assets/icons/email.png";
 import password_icon from "../../assets/icons/password.png";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import Button from "../../components/button/button.jsx";
+import {useContext, useState} from "react";
+import {AuthContext} from "../../context/AuthContext.jsx";
+import axios from "axios";
 
 function Signup() {
+    const {isAuth, toggleIsAuth} = useContext(AuthContext)
+    const [error, toggleError] = useState(false);
+    const [loading, toggleLoading] = useState(false);
+    const navigate = useNavigate();
+
+    function handleChange(e) {
+        const changedFieldName = e.target.name
+
+        toggleIsAuth({
+            ...isAuth,
+            user: {...isAuth.user, [changedFieldName] : e.target.value}
+        })
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        toggleError(false);
+        toggleLoading(true);
+
+        try {
+            await axios.post('http://localhost:3000/register', {
+                email: email,
+                password: password,
+                username: username,
+            });
+            navigate('/signin');
+        } catch(e) {
+            console.error(e);
+            toggleError(true);
+        }
+
+        toggleLoading(false);
+    }
 
     return (
         <div className='container'>
@@ -16,24 +52,24 @@ function Signup() {
             <form onSubmit={handleSubmit}>
                 <div className="inputs">
                     <div className="input">
-                        <img src={user_icon} alt=""/>
+                        <img src={user_icon} alt="afbeelding van persoon"/>
                         <input type="text" placeholder='Gebruikersnaam' id='username' name='username'
                                value={isAuth.user.username} onChange={handleChange}/>
                     </div>
                     <div className="input">
-                        <img src={email_icon} alt=""/>
+                        <img src={email_icon} alt="afbeelding van email"/>
                         <input type="email" placeholder='E-mailadres' id='email' name='email' value={isAuth.user.email}
                                onChange={handleChange}/>
                     </div>
                     <div className="input">
-                        <img src={password_icon} alt=""/>
+                        <img src={password_icon} alt="afbeelding van wachtwoord"/>
                         <input type="password" placeholder='Wachtwoord' id='password' name='password'
                                value={isAuth.user.password} onChange={handleChange}/>
                     </div>
                 </div>
                 {error && <p className="error">Dit account bestaat al. Probeer een ander emailadres.</p>}
                 <div className="submit-container">
-                    <Button type='submit' disabled={loading} className='submit' onClick={handleSubmit}>Aanmelden</Button>
+                    <Button type='submit' disabled={loading} className='submit' onClick={handleSubmit}>Meld mij aan</Button>
                 </div>
                 <div className='signup-container'><p>Heb je al een account? Je kunt je <Link
                     to="/signin">hier</Link> inloggen.</p></div>
@@ -42,5 +78,4 @@ function Signup() {
         </div>
     );
 }
-
 export default Signup;
