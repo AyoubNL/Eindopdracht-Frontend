425aefde45de709e9f154d766e4a2e3fd3e3d210
AyoubNL <gmilio@live.nl>
2024-07-18 02:23:00 +0200

    Added protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './styles/global.css'
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
 import Signin from "./pages/signin/Signin.jsx";
 import Signup from "./pages/signup/Signup.jsx";
@@ -7,8 +7,12 @@ import Fleet from "./pages/fleet/Fleet.jsx";
 import Notfound from "./pages/notfound/Notfound.jsx";
 import NavBar from "./components/navbar/NavBar.jsx";
 import Footer from "./components/footer/Footer.jsx";
+import {useContext} from "react";
+import {AuthContext} from "./context/AuthContext.jsx";
 
 function App() {
+
+    const {isAuth} = useContext(AuthContext);
 
     return (
         <>
@@ -17,7 +21,7 @@ function App() {
                 <Route path='/' element={<Home/>}/>
                 <Route path='/signup' element={<Signup/>}/>
                 <Route path='/signin' element={<Signin/>}/>
-                <Route path='/fleet' element={<Fleet/>}/>
+                <Route path='/fleet' element={isAuth.isAuth ? <Fleet /> : <Navigate to="/signin"/>}/>
                 <Route path="*" element={<Notfound/>}/>
             </Routes>
             <Footer/>
