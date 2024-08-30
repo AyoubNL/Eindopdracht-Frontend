c950815653c80b52ca32d5daedf4a5bf1a08988e
AyoubNL <gmilio@live.nl>
2024-08-30 23:14:25 +0200

    added header

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 
 function Home() {
     return (<>
-            <div className="wrapper">
+            <header>
                 <div className="text-box">
                     <h1>APKdash</h1>
                     <p>Het runnen van een groot wagenpark vraagt om een goede administratie. Als ondernemer heb je
@@ -15,12 +15,12 @@ function Home() {
                         de deurmat voor een verlopen APK-keuring. <Link to='/signup'>Registreer je nu!</Link></p>
                 </div>
                 <img src={logo} alt="afbeelding logo"/>
-            </div>
+            </header>
             <div id='usp-section'>
                 <h2>Voordelen</h2>
                 <div className="cards">
-                    <section className="card">
-                        <article className="content">
+                    <section>
+                        <article>
                             <h3>Razendsnel</h3>
                             <p>De APK-vervaldata van jouw wagenpark is binnen één seconde zichtbaar op het scherm.
                                 Dat kun je heel makkelijk weten welk voertuig naar de autogarage moet. Met APKdash
@@ -28,8 +28,8 @@ function Home() {
                             </p>
                         </article>
                     </section>
-                    <section className="card">
-                        <article className="content">
+                    <section>
+                        <article>
                             <h3>Betrouwbaar</h3>
                             <p>Onze informatie komt rechtstreeks van de RDW voertuiggegevens database en is 100 procent
                                 betrouwbaar.
@@ -37,12 +37,12 @@ function Home() {
                                 paraat. Om te beginnen het merk, model en de APK-vervaldatum.</p>
                         </article>
                     </section>
-                    <section className="card">
-                        <article className="content">
+                    <section>
+                        <article>
                             <h3>Overzichtelijk</h3>
                             <p>Heb je snel een compleet beeld van je wagenpark nodig? Dankzij onze portaal krijg je in
                                 één oogopslag de komende APK keuringen te zien. Het aanhouden van een rommelige
-                                administratie is verleden tijd. </p>
+                                administratie is verleden tijd.</p>
                         </article>
                     </section>
                 </div>
